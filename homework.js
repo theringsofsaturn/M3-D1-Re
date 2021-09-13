318be8bbf24b924d2004b7ab13c36b48d03429ad
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-13 15:59:23 +0200

    ex 4

--- homework.js
+++ homework.js
@@ -27,6 +27,11 @@ const removeChar = function (str, pos) {
 
 /* 4. Create a function to find and return the largest of three given integers. */
 
+const findLargestInt = function (...args) {
+  const sorted = args.sort((a, b) => b - a);
+  return sorted[0];
+};
+
 /* 5. Create a function to check if two numbers are in the range 40-60 or 70-100. 
     Return `true` if they do, return `false` if one (or both) don't. */
 
